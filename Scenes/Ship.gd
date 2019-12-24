14f6fcb5ea54c0a0a4ebb992e3ca916b9e32f221
Marco Cognolato <marco.cognolato.98@gmail.com>
2019-12-24 17:35:06 +0100

    Better Input
    implemented input system using gravity and sensitivity

--- Scenes/Ship.gd
+++ Scenes/Ship.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
 export var speed = 50
+export var gravity = 1
+export var sensitivity = 1
 
-func _ready():
-	pass
+var inputs = Vector2()
 
-func _process(delta):
+func get_raw_inputs():
 	var inputs = Vector2()
 	if Input.is_action_pressed("right"):
 		inputs.x += 1
@@ -16,5 +17,34 @@ func _process(delta):
 		inputs.y -= 1
 	if Input.is_action_pressed("down"):
 		inputs.y += 1
+	return inputs
+
+func get_inputs(delta):
+	var raw = get_raw_inputs()
 	
-	translate(inputs * speed * delta)
+	# x-axis
+	if raw.x == 0:
+		var gravitier = - gravity * delta * sign(inputs.x)
+		if sign(inputs.x) != sign(inputs.x + gravitier):
+			inputs.x = 0
+		if inputs.x != 0.0:
+			inputs.x += gravitier
+	else:
+		inputs.x += raw.x * sensitivity * delta
+	inputs.x = clamp(inputs.x, -1, 1)
+	
+	# y-axis
+	if raw.y == 0:
+		var gravitier = - gravity * delta * sign(inputs.y)
+		if sign(inputs.y) != sign(inputs.y + gravitier):
+			inputs.y = 0
+		if inputs.y != 0.0:
+			inputs.y += gravitier
+	else:
+		inputs.y += raw.y * sensitivity * delta
+	inputs.y = clamp(inputs.y, -1, 1)
+	print(inputs)
+	return inputs
+
+func _process(delta):
+	translate(get_inputs(delta) * speed * delta)
